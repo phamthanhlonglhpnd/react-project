6460b8c018421760c1b5fc4cd119b6332bda6301
longpt <phamthanhlonglhpnd@gmail.com>
2024-06-06 17:58:13 +0700

    fix type

--- src/apis/user.api.ts
+++ src/apis/user.api.ts
@@ -2,7 +2,7 @@ import axiosClient from "../configs/axiosClient";
 import { CommonResponse } from "../types/common.type";
 
 const userApi = {
-  getUsers(page: number): Promise<CommonResponse> {
+  getUsers(page: number): Promise<CommonResponse<any>> {
     const url = `user/get-all?page=${page}`;
     return axiosClient.get(url);
   },
--- src/components/Meeting/Meeting.tsx
+++ src/components/Meeting/Meeting.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const Meeting = () => {
   return (
     <div>Meeting</div>
